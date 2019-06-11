a434c7a85f17cb5b5cd0e08956162ebdece9f32a
unknown <jarekszu2@onet.pl>
2019-06-11 18:14:51 +0200

    dodanie tre

--- src/main/java/packGitBranches/MainGitBranches.java
+++ src/main/java/packGitBranches/MainGitBranches.java
@@ -1,7 +1,17 @@
 package packGitBranches;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MainGitBranches {
     public static void main(String[] args) {
+        System.out.println();
 
+        try(PrintWriter printWriter= new PrintWriter(new FileWriter("data.txt", true))) {
+            printWriter.println("Hallo World!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
